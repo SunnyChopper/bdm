67af37c0479b358fa094becd08af1484a2681fcc
Sunny Singh <ishy.singh@gmail.com>
2018-12-27 01:58:48 -0600

    Cleaned up the footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,60 +4,47 @@
 		<div class="footer-warp">
 			<div class="row">
 				<div class="widget-item">
-					<h4>Contact Info</h4>
-					<ul class="contact-list">
-						<li>1481 Creekside Lane <br>Avila Beach, CA 931</li>
-						<li>+53 345 7953 32453</li>
-						<li>yourmail@gmail.com</li>
-					</ul>
-				</div>
-				<div class="widget-item">
-					<h4>Engineering</h4>
-					<ul>
-						<li><a href="">Applied Studies</a></li>
-						<li><a href="">Computer Engeneering</a></li>
-						<li><a href="">Software Engeneering</a></li>
-						<li><a href="">Informational Engeneering</a></li>
-						<li><a href="">System Engeneering</a></li>
-					</ul>
-				</div>
-				<div class="widget-item">
-					<h4>Graphic Design</h4>
-					<ul>
-						<li><a href="">Applied Studies</a></li>
-						<li><a href="">Computer Engeneering</a></li>
-						<li><a href="">Software Engeneering</a></li>
-						<li><a href="">Informational Engeneering</a></li>
-						<li><a href="">System Engeneering</a></li>
-					</ul>
-				</div>
-				<div class="widget-item">
-					<h4>Development</h4>
-					<ul>
-						<li><a href="">Applied Studies</a></li>
-						<li><a href="">Computer Engeneering</a></li>
-						<li><a href="">Software Engeneering</a></li>
-						<li><a href="">Informational Engeneering</a></li>
-						<li><a href="">System Engeneering</a></li>
-					</ul>
-				</div>
-				<div class="widget-item">
 					<h4>Newsletter</h4>
+					<p>Get tips to become a technology entrepreneur.</p>
 					<form class="footer-newslatter">
 						<input type="email" placeholder="E-mail">
 						<button class="site-btn">Subscribe</button>
 						<p>*We don’t spam</p>
 					</form>
 				</div>
+				<div class="widget-item">
+
+				</div>
+				<div class="widget-item">
+					<h4>Contact Info</h4>
+					<ul class="contact-list">
+						<p><a href=""><i class="fa fa-instagram" aria-hidden="true"></i> Instagram</a></p>
+					</ul>
+				</div>
+				<div class="widget-item">
+					<h4>Free Resources</h4>
+					<ul>
+						<li><a href="/blog">Free Knowledge</a></li>
+					</ul>
+				</div>
+				<div class="widget-item">
+					<h4>Navigation</h4>
+					<ul>
+						<li><a href="/">Home</a></li>
+						<li><a href="/courses">Courses</a></li>
+						<li><a href="/contact">Contact</a></li>
+					</ul>
+				</div>
+				
 			</div>
 		</div>
 	</div>
 	<div class="footer-bottom">
 		<div class="footer-warp">
 			<ul class="footer-menu">
-				<li><a href="#">Terms & Conditions</a></li>
+				{{-- <li><a href="#">Terms & Conditions</a></li>
 				<li><a href="#">Register</a></li>
-				<li><a href="#">Privacy</a></li>
+				<li><a href="#">Privacy</a></li> --}}
 			</ul>
 			<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>

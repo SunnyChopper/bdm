c80eb9ed1327db9f7fe115cc13378ad847cbe0ab
Sunny Singh <ishy.singh@gmail.com>
2018-12-28 04:17:22 -0600

    Default banner text

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,5 +1,9 @@
 <div class="page-info-section set-bg" data-setbg="{{ URL::asset('img/page-bg/4.jpg') }}">
 	<div class="container" style="height: fit-content;">
-		<h2 class="text-center text-white" style="position: relative; top: calc(50% + 0.75em)">{{ $page_header }}</h2>
+		@if(isset($page_header))
+			<h2 class="text-center text-white" style="position: relative; top: calc(50% + 0.75em)">{{ $page_header }}</h2>
+		@else
+			<h2 class="text-center text-white" style="position: relative; top: calc(50% + 0.75em)">Digital Age CEO</h2>
+		@endif
 	</div>
 </div>

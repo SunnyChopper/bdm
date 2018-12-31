590b9b41d935310b33dcd56a51c083dda51d81f5
Sunny Singh <ishy.singh@gmail.com>
2018-12-31 03:24:00 -0600

    Fixed mobile spacing issue

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,9 +1,9 @@
 <div class="page-info-section set-bg" data-setbg="{{ URL::asset('img/page-bg/4.jpg') }}">
-	<div class="container" style="height: fit-content;">
+	<div class="container" style="height: fit-content; display: inline-block; margin: auto;">
 		@if(isset($page_header))
-			<h2 class="text-center text-white" style="position: relative; top: calc(50% + 0.75em)">{{ $page_header }}</h2>
+			<h2 class="text-center text-white">{{ $page_header }}</h2>
 		@else
-			<h2 class="text-center text-white" style="position: relative; top: calc(50% + 0.75em)">Digital Age CEO</h2>
+			<h2 class="text-center text-white">Digital Age CEO</h2>
 		@endif
 	</div>
 </div>

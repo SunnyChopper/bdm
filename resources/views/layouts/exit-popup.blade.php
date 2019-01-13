6e845603237ddf262bbca27ddfc73fd51f5a05ea
Sunny Singh <ishy.singh@gmail.com>
2019-01-12 18:08:29 -0600

    Added cookies and make modal show up

---
+++ resources/views/layouts/exit-popup.blade.php
@@ -0,0 +1,61 @@
+<div id="ouibounce-modal">
+	<div class="underlay"></div>
+	<div class="modal">
+		<div class="modal-title">
+			<h3>But wait...</h3>
+		</div>
+
+		<div class="modal-body">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<h5 style="color: black;" class="text-center">Join others in getting exclusive tips and advice</h5>
+						<hr />
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-6 order-lg-1 col-md-6 order-md-1 col-sm-5 order-sm-1 col-12 order-2 mt-16-mobile" style="display: flex;">
+						<p class="mobile-text-center" style="margin: auto;">Stop making apps and websites that people don't want. Stop working so hard and getting so little in return. Learn and join the many others who are learning how you can make the difference from just knowing how to code to becoming a tech entrepreneur whose work is recognized.</p>
+					</div>
+					<div class="col-lg-6 order-lg-2 col-md-6 order-md-2 col-sm-7 order-sm-2 d-md-block d-none" style="margin: auto;">
+						<img src="https://static1.squarespace.com/static/5429ab2be4b0bc226325e671/t/58b0aacfe3df2844f563e603/1487973082474/" class="regular-image">
+					</div>
+				</div>
+
+				<form id="popup-form">
+					<input type="hidden" id="popup-source" name="source" value="exit-popup">
+					<input type="hidden" id="popup-list_name" name="list_name" value="general">
+    				<div class="row mt-16">
+    					<div class="col-lg-4 ol-md-4 col-sm-12 col-12" style="margin: auto;">
+    						<input type="text" id="popup-name" name="name" placeholder="Name" class="form-control mb-16-mobile" required>
+    					</div>
+
+    					<div class="col-lg-4 ol-md-4 col-sm-12 col-12" style="margin: auto;">
+    						<input type="email" id="popup-email" name="email" placeholder="Email" class="form-control mb-16-mobile" required>
+    					</div>
+
+    					<div class="col-lg-4 ol-md-4 col-sm-12 col-12" style="margin: auto;">
+    						<input type="submit" id="popup-submit" value="Gain Access" class="btn btn-success center-button" required>
+    					</div>
+    				</div>
+    				<div class="row mt-8">
+    					<div class="col-12">
+    						<p id="feedback" class="mb-0 text-center green" style="display: none;">Successfully submitted.</p>
+    					</div>
+    				</div>
+    			</form>
+			</div>
+		</div>
+
+		<div class="modal-footer">
+			<div class="container">
+    			<div class="row">
+    				<div class="col-12">
+		    			<p class="text-center">Nah, I don't care about knowledge</p>
+		    		</div>
+	    		</div>
+	    	</div>
+		</div>
+	</div>
+</div>
--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -4,6 +4,7 @@
 <script src="{{ URL::asset('js/mixitup.min.js') }}"></script>
 <script src="{{ URL::asset('js/circle-progress.min.js') }}"></script>
 <script src="{{ URL::asset('js/owl.carousel.min.js') }}"></script>
+<script src="{{ URL::asset('js/ouibounce.js') }}"></script>
 <script src="{{ URL::asset('js/main.js') }}"></script>
 
 @yield('page_js')

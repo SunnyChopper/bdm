b38c67ff2c95c758b735eb0d6561915634ce8058
Sunny Singh <ishy.singh@gmail.com>
2019-03-21 06:34:34 -0500

    Added email for new users

---
+++ resources/views/emails/new-user.blade.php
@@ -0,0 +1,7 @@
+<h3>Welcome to the Community</h3>
+
+<p>Hi {{ $first_name }},</p>
+<p>We're glad that you've joined the Digital Age CEO community! Our mission is to help coders and aspiring entrepreneurs become successful tech entrepreneurs. Everything we do is aligned with that.</p>
+<p>You can now access your dashboard here with your login information. You can get access to premium content, courses, downloadables, and much more from your dashboard so we recommend that you login now and look around!</p>
+<p>If you have any questions, let us know! Simply reply to this email and we will get back to you!</p>
+<p>Best regards,<br>Digital Age CEO</p>
